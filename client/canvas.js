1ac6a2a79e24a5c47010f0d4a2f8caa7027d2e0e
Victor Mendiluce <vmendi@gmail.com>
2012-09-23 14:58:44 -0700

    -

--- client/canvas.js
+++ client/canvas.js
@@ -1,37 +1,39 @@
-
-function resetCanvas(){
-
+function resetCanvas() { 
   $('#progress01').css("left",60);
   $('#progress02').css("left",60);
 
-
+  currentTime = 0;
+  $("#TimeID").text("00:00.00");
 }
 
+var currentTime = 0;
 
+function daTimer() {
 
-function iniciateCanvas(){
- 
+  currentTime += 33;
 
+  var seconds = currentTime / 1000;
+
+  $("#TimeID").text("00:" + seconds.toFixed(0) + "." + currentTime.toString().substr(seconds.toString().lenght-3, 2));
   
-    
+  setTimeout(daTimer, 33);
+}
 
+function iniciateCanvas() {
   //$('#progress02').clearQueue();
+
+  setTimeout(daTimer, 33);
  
   $('#progress01').animate({
     left: '+=220',
   }, 19000, function() {
     resetCanvas();
     $.mobile.changePage('#ResultsPage', {transition:'none'});
-    
   });
 
-
   $('#progress02').animate({
-    
    left: '+=220',
     }, 20000, function() {
-     
-    
   });
 
 
