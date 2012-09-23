fee0dd64e0d2e94c7b34349e6d191297b3dd4571
ingew <ignacio.villar.munoz@gmail.com>
2012-09-23 14:08:42 -0700

    8

--- client/canvas.js
+++ client/canvas.js
@@ -1,16 +1,33 @@
 function iniciateCanvas(){
  
+  $('#progress01').stop();
+  $('#progress01').clearQueue();
+   $("#progress01").offset({ left: "60px"});
+
+
+
+
+  $(".layer2").position("left : '60px' ");
+
+
+  
+    
+
+  //$('#progress02').clearQueue();
+ 
   $('#progress01').animate({
     left: '+=220',
   }, 19000, function() {
+    
     
   });
 
 
   $('#progress02').animate({
+    
    left: '+=220',
     }, 20000, function() {
-      // Animation complete.
+    
   });
 
 
