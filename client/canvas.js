b9e0053021e68bf7d9a5e7b2d9451659798af67a
ingew <ignacio.villar.munoz@gmail.com>
2012-09-23 13:00:28 -0700

    7

--- client/canvas.js
+++ client/canvas.js
@@ -1,2 +1,17 @@
 function iniciateCanvas(){
+ 
+  $('#progress01').animate({
+    left: '+=220',
+  }, 19000, function() {
+    
+  });
+
+
+  $('#progress02').animate({
+   left: '+=220',
+    }, 20000, function() {
+      // Animation complete.
+  });
+
+
 }
