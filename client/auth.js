9d7585edaaec5637130d5830fac9bab4d91742a2
Victor Mendiluce <vmendi@gmail.com>
2012-09-23 14:11:00 -0700

    -

--- client/auth.js
+++ client/auth.js
@@ -8,6 +8,9 @@
 var GlobalUser = [];
 var GlobalFriends = [];
 
+var GlobalOppId = null;
+var GlobalOppName = null;
+
 (function() {
   var e = document.createElement('script'); e.async = true;
   e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
